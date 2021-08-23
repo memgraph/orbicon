a2ae2202cba22e68b142e3f3f8d550b236c19db7
josipmrden <josip.mrden@memgraph.io>
2021-08-23 16:55:42 +0200

    Initialized frontend repository

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<style scoped></style>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data: function () {
+    return {
+      msg: "Hello world works!",
+    };
+  },
+};
+</script>
