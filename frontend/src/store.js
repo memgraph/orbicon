5ad02282aafba5eb38995e6be70baf7f6c73fe2c
josipmrden <josip.mrden@memgraph.io>
2021-08-24 11:21:51 +0200

    Added state management init

---
+++ frontend/src/store.js
@@ -0,0 +1,19 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+const mutations = {
+
+}
+
+const actions = {
+
+}
+
+
+export const store = {
+  mutations,
+  actions,
+  
+}
