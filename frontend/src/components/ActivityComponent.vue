d65cdbee5c19e2b16155b4bb4beeb9af0b35ce63
josipmrden <josip.mrden@memgraph.io>
2021-08-25 14:11:14 +0200

    Styled activities

--- frontend/src/components/ActivityComponent.vue
+++ frontend/src/components/ActivityComponent.vue
@@ -1,10 +1,28 @@
 <template>
-  <div>
-    {{ activity.username }}
+  <div class="card-container">
+    <v-card elevation="16">
+      <v-card-title>
+        {{ activity.username }}
+      </v-card-title>
+      <v-card-subtitle align="left">
+        {{ activity.action }}
+      </v-card-subtitle>
+      <v-card-text>
+        {{ activity.description }}
+      </v-card-text>
+      <v-card-text align="right">
+        {{ activity.date }}
+      </v-card-text>
+    </v-card>
   </div>
 </template>
 
 <style scoped>
+.card-container {
+  margin-bottom: 10px;
+  margin-left: 10px;
+  margin-right: 10px;
+}
 </style>
 
 
