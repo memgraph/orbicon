723ad15ec2c856c00d83dcf7ab1baf244f741bff
josipmrden <josip.mrden@memgraph.io>
2021-08-27 10:02:18 +0200

    Member graph created and double click fixed

--- frontend/src/api/apiClient.js
+++ frontend/src/api/apiClient.js
@@ -2,20 +2,38 @@ import Vue from 'vue';
 
 export const API_URL = 'http://localhost:3000';
 
+export const apiRoutes = {
+  memberGraphRoute() {
+    return `${API_URL}/memberGraph`
+  },
+  usernamesRoute() {
+    return `${API_URL}/usernames`
+  },
+  usernamesWithPrefixRoute(prefix) {
+    return `${API_URL}/usernamesWithPrefix/${prefix}`
+  },
+  userDetailsRoute(username) {
+    return `${API_URL}/userDetails/${username}`
+  },
+  activitiesRoute() {
+    return `${API_URL}/activities`
+  },
+}
+
 export const apiClient = {
   getMemberGraph: () => {
-    return Vue.axios.get(`${API_URL}/memberGraph`);
+    return Vue.axios.get(apiRoutes.memberGraphRoute());
   },
   getUsernames: () => {
-    return Vue.axios.get(`${API_URL}/usernames`);
+    return Vue.axios.get(apiRoutes.usernamesRoute());
   },
   getUsernamesWithPrefix: (prefix) => {
-    return Vue.axios.get(`${API_URL}/usernamesWithPrefix/${prefix}`);
+    return Vue.axios.get(apiRoutes.usernamesWithPrefixRoute(prefix));
   },
   getUserDetails: (username) => {
-    return Vue.axios.get(`${API_URL}/userDetails/${username}`);
+    return Vue.axios.get(apiRoutes.userDetailsRoute(username));
   },
   getActivities: () => {
-    return Vue.axios.get(`${API_URL}/activities`);
+    return Vue.axios.get(apiRoutes.activitiesRoute());
   },
 }
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,10 +5,10 @@ import { store } from './store/store.js';
 
 Vue.config.productionTip = false
 
-// if (process.env.NODE_ENV === 'development') {
-//   const { worker } = require('./mocks/browser')
-//   worker.start()
-// }
+if (process.env.NODE_ENV === 'development') {
+  const { worker } = require('./mocks/browser')
+  worker.start()
+}
 
 new Vue({
   vuetify,
