6163d9bbb7e64a464a5fe1944edb4f58d40a733d
fico <filipovicantonio1998@gmail.com>
2021-08-27 13:05:41 +0200

    add pagerank

--- modules/orbit_graph_calculus.py
+++ modules/orbit_graph_calculus.py
@@ -1,6 +1,8 @@
 import mgp
-import kmeans
+import networkx as nx
 
+import kmeans
+from mgp_networkx import PropertiesDictionary
 
 
 @mgp.read_proc
@@ -28,3 +30,41 @@ def get_labels(ctx: mgp.ProcCtx, nodes: mgp.List[mgp.Vertex], embeddings: mgp.Li
         mgp.Record(node=node, label=int(label))
         for node, label in nodes_labels_list
     ]
+
+
+@mgp.read_proc
+def pagerank(
+    ctx: mgp.ProcCtx,
+    vertices: mgp.List[mgp.Vertex],
+    edges: mgp.List[mgp.Edge]
+) -> mgp.Record(node=mgp.Vertex, rank=float):
+    def to_properties_dictionary(prop):
+        return None if prop is None else PropertiesDictionary(ctx, prop)
+
+    alpha= 0.85
+    personalization = None
+    max_iter = 100
+    tol = 1e-06
+    nstart = None
+    weight= "weight"
+    dangling = None
+
+    print(vertices)
+
+    g = nx.DiGraph()
+    g.add_nodes_from(vertices)
+    g.add_edges_from([(edge.from_vertex, edge.to_vertex) for edge in edges])
+    g.add_edges_from([(edge.from_vertex, edge.to_vertex) for edge in edges])
+
+    pg = nx.pagerank(
+        g,
+        alpha=alpha,
+        personalization=to_properties_dictionary(personalization),
+        max_iter=max_iter,
+        tol=tol,
+        nstart=to_properties_dictionary(nstart),
+        weight=weight,
+        dangling=to_properties_dictionary(dangling),
+    )
+
+    return [mgp.Record(node=k, rank=v) for k, v in pg.items()]
