7877c71f4587d8e90fd825b24d8c0b021a396ba9
josipmrden <josip.mrden@memgraph.io>
2021-09-07 11:57:11 +0200

    Styled activity card

--- frontend/src/components/ActivityComponent.vue
+++ frontend/src/components/ActivityComponent.vue
@@ -3,24 +3,28 @@
     <v-card elevation="16">
       <v-list-item three-line>
         <v-list-item-content>
-          <div class="text-overline mb-4">{{ activity.action }}</div>
+          <div class="text-overline mb-4">
+            <v-list-item-subtitle>
+              {{ activity.action }}
+            </v-list-item-subtitle>
+          </div>
           <v-list-item-title class="text-h5 mb-1">
             {{ activity.username }}
           </v-list-item-title>
-          <v-btn :href="activity.url" target="_blank">
-            Check out activity!
-          </v-btn>
+          <v-card-text class="card-date">
+            {{ dateFormatted }}
+          </v-card-text>
         </v-list-item-content>
 
         <v-list-item-avatar size="80" color="grey"
           ><v-img :src="activity.avatar"></v-img
         ></v-list-item-avatar>
       </v-list-item>
-      <v-list-item one-line>
-        <v-btn align="left" @click="onBtnClick">Check user!</v-btn>
-        <v-card-text align="right">
-          {{ activity.date }}
-        </v-card-text>
+      <v-list-item class="btn-list-item">
+        <v-btn align="left" @click="onBtnClick">Check user</v-btn>
+        <v-btn align="right" :href="activity.url" target="_blank">
+          Check activity
+        </v-btn>
       </v-list-item>
     </v-card>
   </div>
@@ -31,6 +35,16 @@
   margin-bottom: 10px;
   margin-left: 10px;
   margin-right: 10px;
+}
+
+.btn-list-item {
+  display: flex;
+  justify-content: space-between;
+}
+
+.card-date {
+  font-weight: 500;
+  font-size: 16px;
 }
 </style>
 
@@ -57,6 +71,15 @@ export default {
   },
   computed: {
     ...mapGetters(["isFetchingUserDetails"]),
+    dateFormatted() {
+      const date = this.activity.date;
+      const lastDotIndex = date.lastIndexOf(".");
+      const noMilisecondDate = date.substring(0, lastDotIndex);
+      const formattedTimeDate = noMilisecondDate
+        .replace("T", " @ ")
+        .replaceAll("-", "/");
+      return formattedTimeDate;
+    },
   },
 };
 </script>
